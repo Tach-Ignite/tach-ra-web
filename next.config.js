feaaaaa6d0e1e2f20fadfa3e514603227dd2cc77
Jeremy Smart <jersmart@gmail.com>
2024-02-29 22:17:59 -0600

    added additional domain for aws s3 to next config

--- next.config.js
+++ next.config.js
@@ -105,6 +105,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     domains: [
       `${process.env.TACH_AWS_BUCKET_NAME}.s3.${process.env.TACH_AWS_REGION}.amazonaws.com`,
+      `${process.env.TACH_AWS_BUCKET_NAME}.s3.amazonaws.com`,
       'authjs.dev',
       'tachignitelocal11335-local.s3.us-east-1.amazonaws.com',
       'example.s3.us-east-1.amazonaws.com',
